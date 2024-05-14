85eba6d4887d8c093d880cccff64d3e18196dd61
satyamkodale <satyamkodale@gmail.com>
2024-05-14 18:20:17 +0530

    Initial commit: Set up project structure and added initial files

---
+++ src/main/java/com/dailype/task/DailypeTaskApplication.java
@@ -0,0 +1,13 @@
+package com.dailype.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailypeTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailypeTaskApplication.class, args);
+	}
+
+}
