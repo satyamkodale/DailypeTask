d6068031970276f1e5e8bd0b467281b32048b3a2
satyamkodale <satyamkodale@gmail.com>
2024-05-16 11:30:12 +0530

    added validation on USerDto, /create_user endpoint and corrected endpoints as per tasks guidlines

--- src/main/java/com/dailype/task/controller/UserController.java
+++ src/main/java/com/dailype/task/controller/UserController.java
@@ -2,6 +2,8 @@ package com.dailype.task.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,30 +19,41 @@ import com.dailype.task.dtos.ApiResponseMessage;
 import com.dailype.task.dtos.ManagerDto;
 import com.dailype.task.dtos.UpdateUserData;
 import com.dailype.task.dtos.UserDto;
+import com.dailype.task.exceptions.GlobalExceptionHandler;
 import com.dailype.task.services.ManagerService;
 import com.dailype.task.services.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 	
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+   
+
+	
 	  @Autowired
 	    private UserService userService;
 	  
-	    //post mapping 
-	    @PostMapping
-		 public ResponseEntity<UserDto> createUser( @RequestBody UserDto userDto) {
+	    //post mapping create user without manager id 
+	    @PostMapping("/create_user")
+		 public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
+	    	
 		        UserDto createdUser = userService.createUser(userDto);
+		        logger.info("User Created  !!");
 		        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+		        
 		   }
 
-	    //create user with manager 
-	    @PostMapping("/{managerId}")
+	    //create user with manager id 
+	    @PostMapping("/create_user/{managerId}")
 	    public ResponseEntity<UserDto> createUserWithManager(
 	            @PathVariable("managerId") String managerId,
 	            @RequestBody UserDto userDto
 	    ) {
 	        UserDto productWithCategory = userService.createWithManager(userDto, managerId);
+	        logger.info("User Created with manager Id  !!");
 	        return new ResponseEntity<>(productWithCategory, HttpStatus.CREATED);
 	    }
 	    
@@ -48,26 +61,28 @@ public class UserController {
 	    //get mapping 
 	    
 	    // get all users 
-	    @GetMapping
+	    @GetMapping("/get_users")
 	   public ResponseEntity<List<UserDto>> getAllUsers(){
 		   List<UserDto> allDtos = userService.getAllUsers();
+		   logger.info("Got all Users   !!");
 		 return  new ResponseEntity<>(allDtos, HttpStatus.OK);
 		   
 	   }
 	   
 	   //get all by manager id 
 	   
-	    @GetMapping("/ofManager/{managerId}")
+	    @GetMapping("get_users/ofManager/{managerId}")
 	    public ResponseEntity<List<UserDto>> getAllUsersOfManager(
 	            @PathVariable("managerId") String managerId      
 	    ) {
 	        List<UserDto> users = userService.getAllUserOfManager(managerId);
+	     
 	        return new ResponseEntity<>(users, HttpStatus.OK);
 	    }
 	    
 	    //get User  by user id 
 	    
-	    @GetMapping("/userId/{userId}")
+	    @GetMapping("get_users/userId/{userId}")
 	    public ResponseEntity<UserDto> getUserById(
 	            @PathVariable("userId") String userId      
 	    ) {
@@ -77,7 +92,7 @@ public class UserController {
 	   
 	   //get user by mobile number
 	    
-	    @GetMapping("/userMobNum/{mobNum}")
+	    @GetMapping("get_users/userMobNum/{mobNum}")
 	    public ResponseEntity<UserDto> getUserByMobNo(
 	            @PathVariable("mobNum") String mobNum     
 	    ) {
@@ -86,8 +101,8 @@ public class UserController {
 	    }
 	    
 	    
-	    //delete Mapping 
-	    @DeleteMapping("/{userId}")
+	    //delete Mapping user based on userId 
+	    @DeleteMapping("delete_user/{userId}")
 	    public ResponseEntity<ApiResponseMessage> delete(@PathVariable String userId) 
 	    {
 	        userService.delete(userId);
--- src/main/java/com/dailype/task/dtos/UserDto.java
+++ src/main/java/com/dailype/task/dtos/UserDto.java
@@ -11,6 +11,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,14 +24,34 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder
 public class UserDto {   
-	    private String userId;	   
-	    private String userFullName;	    
-	    private String userEmail;	    
-	    private String userMobNum;	    
-	    private String userPanNum;	    
-	    private String userPassword;	    
-	    private LocalDateTime createdAt;
-	    private LocalDateTime updatedAt;
-	    private boolean isActive;
-	    private Manager manager;
+
+       private String userId;
+
+      @NotBlank(message = "Full name must not be empty")
+        private String userFullName;
+
+        @NotBlank(message = "Email must not be empty")
+       // Add Email validation logic here if needed
+         private String userEmail;
+
+       @NotBlank(message = "Mobile number must not be empty")
+	    @Pattern(regexp = "^(0|\\+91)?\\d{10}$", message = "Invalid mobile number")
+        private String userMobNum;
+
+        @NotBlank(message = "PAN number must not be empty")
+        @Pattern(regexp = "^[A-Z]{5}[0-9]{4}[A-Z]$", message = "Invalid PAN number")
+        private String userPanNum;
+
+        @NotBlank(message = "Password must not be empty")
+        private String userPassword;
+
+        private LocalDateTime createdAt;
+
+        private LocalDateTime updatedAt;
+
+        private boolean isActive;
+ 
+        private Manager manager;
+//
+//	    // Getters and setters
 }
